92b0ae264133e04a9e2bebbb143d5a0acca52f01
pauloovitorr <paulov.pv25@gmail.com>
2024-05-03 23:34:59 -0300

    Conexão

---
+++ conn.php
@@ -0,0 +1,18 @@
+<?php 
+
+$host = "zeus.hostsrv.org";
+$user = "comtest_pauloovitorr";
+$password = "csExVgO-wN9x";
+$dd = "comtest_form";
+
+$conexao = new mysqli($host, $user,$password, $dd);
+
+// if($conexao->connect_error){
+//     echo "Deu ruim";
+// }
+// else{
+//     echo "BOAAA";
+// }
+
+
+?>
